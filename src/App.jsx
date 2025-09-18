7af31ff4c305356c91d7ef26bbc41a965a623d28
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2025-09-18 12:23:56 +0700

    search bar and color.css
    the search icon is still error, will comeback later

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './color.css'
+import './header/Header.css'
 
 import Header from './header/Header'
 
