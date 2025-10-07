971ec36ae67b35efac14507f4763cd05afd33885
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2025-10-07 18:50:11 +0700

    done with header

--- src/breadcrumb/Breadcrumb.jsx
+++ src/breadcrumb/Breadcrumb.jsx
@@ -1,13 +1,48 @@
-function Breadcrumb(){
-    return(
+import React from "react";
+import breadcrumbItems from '../mockData/breadcrumbs.json';
+
+const BreadcrumbText = (props) => {
+    return (
+        <>
+            <ol className="BreadcrumbListContainer">
+                <div className="BreadcrumbList">
+                {props.items.map((item, index) => {
+                    const isLast = index === props.items.length - 1;
+
+                    return (
+                        <React.Fragment key={item.label}>
+
+                            {isLast ? (
+                                <li className="listEntry">
+                                    <span>{item.label}</span>
+                                </li>
+                            ) : (
+                                <li className="listEntry">
+                                    <a href={item.link}>
+                                        <span>{item.label}</span>
+                                    </a>
+                                    <div className="slash">
+                                        <pre> / </pre>
+                                    </div>
+                                </li>
+                            )}
+
+                        </React.Fragment>
+                    );
+                })}
+            </div>
+        </ol >
+        </>
+    )
+}
+
+function Breadcrumb() {
+    return (
         <>
             <div className="BreadcrumbBG">
                 <div className="BreadcrumbContainer">
-                    <ol className="BreadcrumbListContainer">
-                        <div className="BreadcrumbList">
-                            
-                        </div>
-                    </ol>
+                    <BreadcrumbText items={breadcrumbItems} />
+                    <h1 className="Title">ร้านอาหาร ยอดนิยม ในกรุงเทพและปริมณฑล</h1>
                 </div>
             </div>
         </>
